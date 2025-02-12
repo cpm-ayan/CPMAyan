1bdb7c15ae06cb8bbe47159bab7c6e1260211e70
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-02-12 13:49:30 +0500

    Update and rename cpmewan1999.py to cpmtooldev.py

--- cpmtooldev.py
+++ cpmtooldev.py
@@ -2,7 +2,7 @@ import requests
 
 __ENDPOINT_URL__: str = "https://cpmayan.squareweb.app/api"
 
-class CPMEwan1999:
+class CPMTooldev:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
