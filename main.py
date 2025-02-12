bb2a99b4d89c5c36d0e7705ba910ddb981fe4b62
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-02-12 19:26:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -161,6 +161,7 @@ import requests
 from time import sleep
 import os, signal, sys
 from rich.console import Console
+from pyfiglet import figlet_format
 from rich.prompt import Prompt, IntPrompt
 from rich.text import Text
 from rich.style import Style
