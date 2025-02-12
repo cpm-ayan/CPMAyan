c5c63e6ab8037163987161ca2734f885bce986d0
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-02-12 19:15:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -127,8 +127,17 @@ banner = r"""
 
 
 ⠀⠀
-                   █░█ ▄▀█ █▀▀ █▄▀ █▀▀ █▀█
-                   █▀█ █▀█ █▄▄ █░█ ██▄ █▀▄
+                   
+ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░  
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓████████▓▒░░▒▓██████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+                                                      
+                                                      
+
                        
                    𝙲𝙰𝚁 𝙿𝙰𝚁𝙺𝙸𝙽𝙶 𝙼𝚄𝙻𝚃𝙸𝙿𝙻𝙰𝚈𝙴𝚁
                          𝙿𝚁𝙴𝚂𝚂 𝙴𝙽𝚃𝙴𝚁                                 
@@ -316,7 +325,7 @@ if __name__ == "__main__":
             load_key_data(cpm)
             load_client_details()
             choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33"]
-            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube( '                             𝑊𝐸𝐿𝐶𝑂𝑀𝐸 𝑇𝑂 𝑈𝑆𝐸 𝑀𝑌 𝑇𝑂𝑂𝐿\n\n             𝑁𝑂𝑇𝐸: 𝑇𝐻𝐸 𝑈𝑁𝐿𝐼𝑀𝐼𝑇𝐸𝐷 𝐵𝐴𝐿𝐴𝑁𝐶𝐸 𝑂𝑁𝐿𝑌 𝑊𝑂𝑅𝐾𝑆 𝐹𝑂𝑅 𝑂𝑁𝐸 𝑀𝑂𝑁𝑇𝐻\n\n\n01: Unlock Paid Cars           [3.500K] & 02: Increase Money          [1.000K]\n\n\n03: Unlock Coin Cars           [3.000K] & 04: Increase Coins          [3.000K]\n\n\n05: Unlock All Cars            [4.000K] & 06: King Rank               [3.500K]\n\n\n07: Unlock all Cars Siren      [3.500K] & 08: Change ID               [2.500K]\n\n\n09: Unlock w16 Engine          [3.000K] & 10: Change Name             [1..00K]\n\n\n11: Unlock All Horns           [3.000K] & 12: Change Name (Rainbow)   [1..00K]\n\n\n13: Unlock Disable Damage      [2.000K] & 14: Number Plates           [2.000K]\n\n\n15: Unlock Unlimited Fuel      [2.000K] & 16: Account Delete          [F.REE.]\n\n\n17: Unlock All Wheels          [2.500K] & 18: Account Register        [F.REE.]\n\n\n19: Unlock House 3             [2.500K] & 20: Delete Friends          [5..00K]\n\n\n21: Unlock Smoke               [2.000K] & 22: Change Race Wins        [7..00K]\n\n\n23: Change Race Loses          [7..00K] & 24: Custom Engine           [4.000K]\n\n\n25: remove car bumper (Car_ID) [2.000K] & 26: Speed Car Hack (Car_ID) [1.500K]\n\n\n27: Speed All Cars Hack        [2.500K] & 28: Chrome All Cars         [3.500K]\n\n\n29: All Cars Max Milage        [2.000K] & 30: Clone Account           [5.000K]\n\n\n31: Unlock All Tuning          [1.000K] & 32: Steering Angle (Car_ID) [1.500K]\n\n\n                         𝑇𝐸𝐿𝐸𝐺𝑅𝐴𝑀 𝐵𝑂𝑇:- @CPMEwan1999Bot\n\n       𝑈𝑁𝐿𝐼𝑀𝐼𝑇𝐸𝐷 𝐵𝐴𝐿𝐴𝑁𝐶𝐸 𝐹𝑂R 𝐸𝑉𝐸𝑅𝑌 𝑃𝐸𝑅𝑆𝑂𝑁 𝑊𝐻𝑂 𝐴𝐷𝐷𝑆 100 𝑃𝐸𝑂𝑃𝐿𝐸 𝑇𝑂 𝑀𝑌 𝐺𝑅𝑂𝑈𝑃'))))
+            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube( '                             𝑊𝐸𝐿𝐶𝑂𝑀𝐸 𝑇𝑂 𝑈𝑆𝐸 𝑀𝑌 𝑇𝑂𝑂𝐿\n\n             𝑁𝑂𝑇𝐸: 𝑇𝐻𝐸 𝑈𝑁𝐿𝐼𝑀𝐼𝑇𝐸𝐷 𝐵𝐴𝐿𝐴𝑁𝐶𝐸 𝑂𝑁𝐿𝑌 𝑊𝑂𝑅𝐾𝑆 𝐹𝑂𝑅 𝑂𝑁𝐸 𝑀𝑂𝑁𝑇𝐻\n\n\n01: Unlock Paid Cars           [3.500K] & 02: Increase Money          [1.000K]\n\n\n03: Unlock Coin Cars           [3.000K] & 04: Increase Coins          [3.000K]\n\n\n05: Unlock All Cars            [4.000K] & 06: King Rank               [3.500K]\n\n\n07: Unlock all Cars Siren      [3.500K] & 08: Change ID               [2.500K]\n\n\n09: Unlock w16 Engine          [3.000K] & 10: Change Name             [1..00K]\n\n\n11: Unlock All Horns           [3.000K] & 12: Change Name (Rainbow)   [1..00K]\n\n\n13: Unlock Disable Damage      [2.000K] & 14: Number Plates           [2.000K]\n\n\n15: Unlock Unlimited Fuel      [2.000K] & 16: Account Delete          [F.REE.]\n\n\n17: Unlock All Wheels          [2.500K] & 18: Account Register        [F.REE.]\n\n\n19: Unlock House 3             [2.500K] & 20: Delete Friends          [5..00K]\n\n\n21: Unlock Smoke               [2.000K] & 22: Change Race Wins        [7..00K]\n\n\n23: Change Race Loses          [7..00K] & 24: Custom Engine           [4.000K]\n\n\n25: remove car bumper (Car_ID) [2.000K] & 26: Speed Car Hack (Car_ID) [1.500K]\n\n\n27: Speed All Cars Hack        [2.500K] & 28: Chrome All Cars         [3.500K]\n\n\n29: All Cars Max Milage        [2.000K] & 30: Clone Account           [5.000K]\n\n\n31: Unlock All Tuning          [1.000K] & 32: Steering Angle (Car_ID) [1.500K]\n\n\n                         𝑇𝐸𝐿𝐸𝐺𝑅𝐴𝑀 𝐵𝑂𝑇:- @CPMEwan1999Bot\n\n       𝑈𝑁𝐿𝐼𝑀𝐼𝑇𝐸𝐷 𝐵𝐴𝐿𝐴𝑁𝐶𝐸 𝐹𝑂R 𝐸𝑉𝐸𝑅𝑌 𝑃𝐸𝑅𝑆𝑂𝑁 𝑊𝐻𝑂 𝐴𝐷𝐷𝑆 150 𝑃𝐸𝑂𝑃𝐿𝐸 𝑇𝑂 𝑀𝑌 𝐺𝑅𝑂𝑈𝑃'))))
             print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube(  ' ➩{33}: GO TO ANOTHER ACCOUNT'))))
             print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube(  ' ➩{0}: Exit'))))
             print(Colorate.Horizontal(Colors.rainbow, '                         ─═══════════════[ ☆SERVICE☆ ]═══════════════─'))
@@ -1483,7 +1492,7 @@ if __name__ == "__main__":
 
 
             elif service == 15: # Unlimited Fuel
-                console.print("[red][%] UNLOCKING UNLIMITED FUEL[/red]: ", end=None)
+                console.print("[red][%] Unlocking FUEL[/red]: ", end=None)
                 if cpm.unlimited_fuel():
                     print(Colorate.Horizontal(Colors.rainbow, 'SUCCESSFUL'))
                     answ = Prompt.ask("[red][?] DO YOU WANT TO EXIT[/red] ?", choices=["y", "n"], default="n")
