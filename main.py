737e4773d191a36402f3c96da985fa7697b29dc9
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-02-14 20:51:53 +0500

    Update and rename ayan.py to main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 from pyfiglet import figlet_format
-from cpmnuker import CPMNuker
+from cpmayan import CPMAyan
 
-__CHANNEL_USERNAME__ = "Whatsapp 966597761240"
-__GROUP_USERNAME__   = "Whatsapp 966597761240"
+__CHANNEL_USERNAME__ = "cp_ayan" 
+__GROUP_USERNAME__   = "cp_ayan_chat"
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -39,7 +39,7 @@ def gradient_text(text, colors):
 
 def banner(console):
     os.system('cls' if os.name == 'nt' else 'clear')
-    brand_name = figlet_format('AYAN', font='bloody')
+    brand_name = figlet_format('CPMAYAN', font='bloody')
     colors = [
         "rgb(255,0,0)", "rgb(255,69,0)", "rgb(255,140,0)", "rgb(255,215,0)", "rgb(173,255,47)", 
         "rgb(0,255,0)", "rgb(0,255,255)", "rgb(0,191,255)", "rgb(0,0,255)", "rgb(139,0,255)",
@@ -133,7 +133,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CPMNuker(acc_access_key)
+        cpm = CPMAyan(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
