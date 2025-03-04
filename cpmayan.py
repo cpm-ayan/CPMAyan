12e5fc6f6e8c5019a3c0218d55545fd946206d03
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-03-04 13:02:49 +0500

    Update cpmayan.py

--- cpmayan.py
+++ cpmayan.py
@@ -1,6 +1,6 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://cpmayanshop.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://cpmayyan.squareweb.app/api"
 
 class CPMAyan:
     def __init__(self, access_key) -> None:
