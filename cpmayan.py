ed416a24bd313035be577de0434d8232868fc189
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-03-04 15:59:11 +0500

    Update and rename cpmnuker.py to cpmayan.py

--- cpmayan.py
+++ cpmayan.py
@@ -2,7 +2,7 @@ import requests
 
 __ENDPOINT_URL__: str = "https://cpmnuker.squareweb.app/api"
 
-class CPMNuker:
+class CPMAyan:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
