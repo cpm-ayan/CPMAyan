8a53048e558944bbdfd5bcca78a4799f9e7404fb
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-03-05 15:51:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def banner(console):
     ]
     colorful_text = gradient_text(brand_name, colors)
     console.print(colorful_text)
-- [ ]     console.print("[bold blue]♕ CPMAyan♕ [bold blue]: Car Parking Multiplayer Hacking Tool.")
+    console.print("[bold blue]♕ CPMAyan♕ [bold blue]: Car Parking Multiplayer Hacking Tool.")
     console.print(f"[bold green]♕ Telegram[/bold green]: [bold blue]@{__CHANNEL_USERNAME__}[/bold blue] or [bold blue]@{__GROUP_USERNAME__}[/bold blue].")
     console.print("[bold red]==================================================[/bold red]")
     console.print("[bold yellow]! Note[/bold yellow]: Logout from CPM before using this tool !.", end="\n\n")
