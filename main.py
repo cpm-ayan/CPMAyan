7b21dd4e9b1eac082516a49dcc668eee946cb84e
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-03-12 07:25:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,6 @@ def gradient_text(text, colors):
 
 def banner(console):
     os.system('cls' if os.name == 'nt' else 'clear')
-    except:
-        console.clear()
         "rgb(255,0,0)", "rgb(255,69,0)", "rgb(255,140,0)", "rgb(255,215,0)", "rgb(173,255,47)", 
         "rgb(0,255,0)", "rgb(0,255,255)", "rgb(0,191,255)", 
     colorful_text = gradient_text(brand_name, colors)
