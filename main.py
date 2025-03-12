23b0929bc3316697b92c02328bb758625dba3cf9
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-03-12 07:32:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-import cpmtooldevz
+import ayandev
 import base64
 from time import sleep
 import os, signal, sys
@@ -98,7 +98,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = cpmtooldevz.CPMTooldevz(acc_access_key)
+        cpm = Ayandev.CPMTooldevz(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
