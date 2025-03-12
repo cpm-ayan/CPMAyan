e848c5b95239ee208bbb9575d5e2680b5a897929
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-03-12 07:36:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import random
-import CPMAyan
-import base64
+import requests
 from time import sleep
 import os, signal, sys
 from rich.console import Console
 from rich.prompt import Prompt, IntPrompt
 from rich.text import Text
 from rich.style import Style
+import pystyle
+from pystyle import Colors, Colorate
 
+from ayandev import Ayandev
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -98,7 +100,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CPMAyan(acc_access_key)
+        cpm = Ayandev(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
