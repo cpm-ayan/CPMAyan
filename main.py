9aa15c85f02c98965c8bf2293587dbd0ba71580d
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-05-30 12:38:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
             load_player_data(cpm)
             load_key_data(cpm)
             load_client_details()
-            choices = ["00", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58"]
+            choices = ["00", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"]
             console.print("[bold cyan][bold white](01)[/bold white]: Increase Money                 [bold red]1.5K[/bold red][/bold cyan]")
             console.print("[bold cyan][bold white](02)[/bold white]: Increase Coins                 [bold red]1.5K[/bold red][/bold cyan]")
             console.print("[bold cyan][bold white](03)[/bold white]: King Rank                      [bold red]8K[/bold red][/bold cyan]")
